1ea9ec461e1f4fbd78c73d0f1e1be2f6703acf72
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-01-12 08:33:39 +0200

    Initial Commit

---
+++ CoolApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolApp
+//
+//  Created by Teodor Dermendzhiev on 12.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
