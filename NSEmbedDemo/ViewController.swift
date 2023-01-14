834f411b9de078d4190f008dc6eb0144f2c8bf9c
Teodor Dermendjiev <tdermendjievft@gmail.com>
2023-01-14 11:17:37 +0200

    Initial Commit

---
+++ NSEmbedDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NSEmbedDemo
+//
+//  Created by Teodor Dermendzhiev on 14.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
